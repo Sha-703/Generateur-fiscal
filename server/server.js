942d13e185aa2da8aef78944b22495b590e2b4a5
Sha703 <shrlongo@gmail.com>
2026-06-07 14:59:28 +0100

    Ajout du server

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const db = require('./database');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const JWT_SECRET = 'dgrkc-admin-secret-key-2026';
 
 // Middleware
